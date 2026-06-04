9dbf9e498a26d0ad32beb4679b795a9ed49ca51f
ualikhandulat <ualihandulat@gmail.com>
2026-06-04 10:05:42 +0500

    seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,9 +2,11 @@
 
 namespace Database\Seeders;
 
+use App\Enums\Role;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -15,11 +17,14 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        if ( User::doesntExist() ) {
+            User::create([
+                'role' => Role::Administrator,
+                'name' => "Dulat",
+                'login' => "87074432113",
+                'iin' => "000000000000",
+                'password' => Hash::make("123456"),
+            ]);
+        }
     }
 }
